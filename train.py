38aa0575d20c176e922e5f98ae94e8a952f1a826
Pasin <jobpasin@hotmail.com>
2019-05-21 20:34:49 +0900

    Optimize parameters in train.py
    Now adapting to coor_train

--- train.py
+++ train.py
@@ -122,7 +122,8 @@ def run(model_params=None):
                 label_hist[key] = int(val)
     total = label_hist['1'] + label_hist['3'] + label_hist['5']
 
-    model_params['loss_weight'] = [5, 1, 1.8] # Only for BL_361 data: real ratio (22:1:1.8)
+    # model_params['loss_weight'] = [5, 1, 1.8] # Only for BL_361 data: real ratio (22:1:1.8)
+    model_params['loss_weight'] = [5, 1.43, 1] # Only for MD_361 data: real ratio (43:1.43:1)
     # model_params['loss_weight'] = [total / label_hist['1'], total / label_hist['3'], total / label_hist['5']]
     print("Getting training data from %s" % train_data_path)
     print("Saved model at %s" % run_params['result_path'])
